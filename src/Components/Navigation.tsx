54f83af4db07593037f7fe307816a6b1db773e42
Pradeep.D.M <pradeep.mahadeva@hm.com>
2023-03-10 17:34:20 +0530

    name change

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -5,7 +5,7 @@ export const Navigation = (): JSX.Element => {
         <>
             < nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" >
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                    <span className="d-block d-lg-none">Clarence Taylor</span>
+                    <span className="d-block d-lg-none">Pradeep D M</span>
                     <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={dp} alt="..." /></span>
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
